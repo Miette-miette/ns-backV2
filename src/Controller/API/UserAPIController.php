455734672506e9649bbb2b6ff0063c6db6892419
miette <leguyader.doriane@gmail.com>
2025-08-04 23:29:20 +0200

    fix userme authentification

--- src/Controller/API/UserAPIController.php
+++ src/Controller/API/UserAPIController.php
@@ -10,33 +10,29 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class UserAPIController extends AbstractController
 {
     #[Route('/api/userme', name: 'api_me', methods: ['GET'])]
+    #[IsGranted('IS_AUTHENTICATED_REMEMBERED')] 
+    public function me(): JsonResponse
+    {
+        /** @var \App\Entity\User|null $user */
+        $user = $this->getUser();
 
-public function me(): JsonResponse
-{
-    /** @var \App\Entity\User|null $user */
-    $user = $this->getUser();
+        if (!$user) {
+            return $this->json(['error' => 'Not authenticated'], 401);
+        }
 
-    dump($user);
-    dump(get_class($user));
-    die();
-
-    if (!$user) {
-        return $this->json(['error' => 'Not authenticated'], 401);
+        try {
+            return $this->json([
+                'user' => [
+                    'id' => $user->getId(),
+                    'username' => $user->getFirstName(),
+                    'email' => $user->getEmail()
+                ]
+            ]);
+        } catch (\Throwable $e) {
+            return $this->json([
+                'error' => 'Erreur interne',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
-
-    try {
-        return $this->json([
-            'user' => [
-                'id' => $user->getId(),
-                'username' => $user->getFirstName(),
-                'email' => $user->getEmail()
-            ]
-        ]);
-    } catch (\Throwable $e) {
-        return $this->json([
-            'error' => 'Erreur interne',
-            'message' => $e->getMessage(),
-        ], 500);
-    }
-}
 }
